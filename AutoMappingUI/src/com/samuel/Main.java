a333c39a19a4a7197713eb353de8ab4767ba5d6d
haveANiceDay33 <munrosamuel@gmail.com>
2018-05-04 10:04:51 -0400

    removed a "fix" i thought I made.. might have problems in the future

--- AutoMappingUI/src/com/samuel/Main.java
+++ AutoMappingUI/src/com/samuel/Main.java
@@ -412,7 +412,7 @@ public class Main extends HvlTemplateInteg2D{
 							angleOff+=180;
 						}
 						if(waypoints.get(i-1).type.contains("backwards") && waypoints.get(i).type.contains("backwards")) {
-							angleOff -= 360;
+							//angleOff -= 360;
 						}
 						if(angleOff <= -360) {
 							angleOff+=360;
